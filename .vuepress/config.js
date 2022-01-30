6c3c123957b80e34a36836455fbc678dea2a68a8
apparentvisuals <3787826+apparentvisuals@users.noreply.github.com>
2022-01-30 20:28:56 +0000

    chore: update to vuepress 2

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,11 +1,14 @@
 module.exports = {
   title: 'Buildmetric',
   description: 'Customer satisfaction software for home builders',
+  port: 3000,
   themeConfig: {
     search: false,
     title: false,
+    lastUpdated: false,
+    contributors: false,
     logo: '/img/logo-small.png',
-    nav: [
+    navbar: [
       { text: 'Home', link: '/' },
       { text: 'About Us', link: '/about' },
       { text: 'Contact Us', link: '/contact' },
